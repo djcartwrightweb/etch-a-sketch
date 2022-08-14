f0075ed1be7c5a6a5b8349e19e0a4284a7e43f67
djcartwrightweb <dj.cartwright.web@gmail.com>
2022-08-14 16:40:08 -0400

    board working properly

--- scripts.js
+++ scripts.js
@@ -1,24 +1,34 @@
 //create board
 function createBoard() {
-    const board = document.getElementById('board');
-
-    let row = createRow;
+    for (let i = 0; i < 16; i++) {
+        let row = createRow();
+        console.log(row);
+        document.getElementById('board').appendChild(row);
+    }   
 }
+    
 
 function createRow() {
+
     let newRow = document.createElement('div');
     newRow.classList.add('row');
-
-    //create loop to create 10 columns per row
-    for (let i = 0; i < 10; i++) {
-        //left off here Davey
+    // create loop to create 10 columns per row
+    for (let i = 0; i < 16; i++) {
+        newRow.appendChild(createCol());
     }
+    return newRow;
 }
+
 
 function createCol() {
     let newCol = document.createElement('div');
     newCol.classList.add('col');
-
+    newCol.addEventListener('mouseenter',(e) => {
+        newCol.style.backgroundColor = 'black';
+    });
+    newCol.addEventListener('mouseleave', (e) => {
+        setTimeout(() => {newCol.style.backgroundColor = 'white'}, 200);
+    });
     return newCol;
 }
 
